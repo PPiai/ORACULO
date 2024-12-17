f8dd73583048938ace19ee2468529d6db7a96359
PPiai <166030271+PPiai@users.noreply.github.com>
2024-12-17 10:29:26 -0300

    Update 00_Oráculo.py

--- 00_Oráculo.py
+++ 00_Oráculo.py
@@ -63,8 +63,7 @@ def pagina_chat():
 
     chain = st.session_state.get('chain')
     if chain is None:
-        st.error('Para não ocorrer erros, envie uma mensagem como "olá" para inicializar a memória do chat')
-        st.error('Carregue o Oráculo')
+        st.error(f'Carregue o Oráculo \nPara não ocorrer erros, envie uma mensagem como "olá" para inicializar a memória do chat')
         st.stop()
 
     memoria = st.session_state.get('memoria', MEMORIA)
