051c59ed433437a193b88d613b6610f856da56e9
PPiai <166030271+PPiai@users.noreply.github.com>
2024-12-18 08:54:45 -0300

    Update 00_Oráculo.py

--- 00_Oráculo.py
+++ 00_Oráculo.py
@@ -39,7 +39,7 @@ de um documento {tipo_arquivo}:
 {arquivo}
 ####
 
-Utilize as informações fornecidas para basear as suas respostas.
+Utilize as informações fornecidas para basear as suas respostas, você não pode procurar por informações fora de seus documentos dispolibilizados.
 
 Sempre que houver $ na sua saída, substita por R$.
 
