1870b5d0e1e24d39180cbd0beba05dbb13b84f51
PPiai <166030271+PPiai@users.noreply.github.com>
2024-12-18 09:00:10 -0300

    Update 00_Oráculo.py

--- 00_Oráculo.py
+++ 00_Oráculo.py
@@ -103,7 +103,8 @@ def sidebar():
                     'https://v4-ferraz.notion.site/Distribui-o-de-M-dia-Estrat-gias-2-4565b7a4f87649e383c1bed17ed3a505?pvs=4',
                     'https://v4-ferraz.notion.site/Rotinas-168ed1e2971442fabd9d48988fed3202?pvs=4',
                     'https://v4-ferraz.notion.site/Padr-o-de-UTMs-01b41041e864469ab047b5a29b76b512?pvs=73',
-                    'https://v4-ferraz.notion.site/GROWTH-fbffa4424ce14b1d87457b9737706be2']
+                    'https://v4-ferraz.notion.site/GROWTH-fbffa4424ce14b1d87457b9737706be2',
+                    'https://www.notion.so/v4-ferraz/V4-Ferraz-Comece-por-aqui-53b9d81566af46a481dc0c94018035ef']
     with tabs[1]:
         st.markdown(f'<h5 style="text-align: center;">IA: Groq </h5>', unsafe_allow_html=True)
         provedor = 'Groq'
